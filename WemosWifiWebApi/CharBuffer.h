fd8fece070d26530104b3c0443cda4335f495a25
Lariac <larariag@gmail.com>
2023-11-30 01:12:46 +0100

    Increased memory by 5KB (released from unused buffer).

--- WemosWifiWebApi/CharBuffer.h
+++ WemosWifiWebApi/CharBuffer.h
@@ -1,4 +1,4 @@
 // Main multiuse char buffer.
-const unsigned int charBufferSize = 10000;
+const unsigned int charBufferSize = 5000;
 unsigned int charBufferPosition = 0;
 char charBuffer[charBufferSize] = { "\0" };
